c5a220a6addeb4d0ac9f9302d5a186c85fb69904
Andrew Burgess <andrew.burgess@embecosm.com>
2015-06-03 16:00:40 +0100

    06: New test.
    This new test is an extension of test-05, in this test I've added an
    object on the stack in a frame that will be unwound as part of the
    exception throw.  A print in the object destructor show that the object
    is being destroyed.

---
+++ 06/test-06.C
@@ -0,0 +1,57 @@
+#include <cstdio>
+
+class Object
+{
+public:
+  Object ()
+  {
+    printf ("Creating an instance of Object, this = %p\n", this);
+  }
+
+  ~Object ()
+  {
+    printf ("Deleting an instance of Object, this = %p\n", this);
+  }
+};
+
+volatile int i = 0;
+
+__attribute__ ((noinline))
+void
+function ()
+{
+  printf ("Entering function.\n");
+  if (i == 0)
+    throw 1;
+  printf ("Leaving function.\n");
+}
+
+__attribute__ ((noinline))
+void
+wrapper (volatile int *val)
+{
+  Object obj;
+
+  *val = 3;
+  printf ("Entering wrapper (val = %d)\n", *val);
+  function ();
+  printf ("Leaving wrapper (val = %d)\n", *val);
+}
+
+int
+main ()
+{
+  volatile int val = 4;
+  printf ("Entering main (val = %d)\n", val);
+  try
+    {
+      wrapper (&val);
+    }
+  catch (int ex)
+    {
+      printf ("Got an exception: %d\n", ex);
+    }
+
+  printf ("Done (val = %d)\n", val);
+  return 0;
+}
